347accf1e7d2b09416e48deae9ce72512d2c2d2d
Frank <lee0324@126.com>
2017-06-20 19:53:40 +0800

    :checkered_flag: openBrowser

--- bin/server.js
+++ bin/server.js
@@ -1,14 +1,22 @@
-const Koa = require('koa')
-const app = new Koa()
+const Koa = require('koa');
+const app = new Koa();
+
+// var path = require('path');
+// 增加自动在浏览器中打开
+var openBrowser = require('react-dev-utils/openBrowser');
 
 module.exports = function(port) {
-  require('../lib/middleware/webpack')(app)
-  require('../lib/middleware/sass')(app)
-  require('../lib/middleware/views')(app)
-  require('../lib/middleware/router')(app)
-  require('../lib/middleware/static')(app)
-
-  app.listen(port, () => {
-    console.log(`http://127.0.0.1:${port}`)
-  })
-}
+	require('../lib/middleware/webpack')(app);
+	require('../lib/middleware/sass')(app);
+	require('../lib/middleware/views')(app);
+	require('../lib/middleware/router')(app);
+	require('../lib/middleware/static')(app);
+	
+	const locaUrl = `http://127.0.0.1:${port}`;
+	app.listen(port, () => {
+		console.log(locaUrl);
+		if (openBrowser(locaUrl)) {
+			console.log('The browser tab has been opened!');
+		}
+	});
+};
